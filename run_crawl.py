44394a7f41aed4bf2a4742535d72cb82fa260591
Kyle Yap <famasboy88@gmail.com>
2017-11-26 03:38:42 +0800

    Added a small test for scraping

---
+++ run_crawl.py
@@ -0,0 +1,18 @@
+from twisted.internet import task
+from twisted.internet import reactor
+from zywie_pinoy_scraper.spiders.firebasetest import FirebasetestSpider
+from scrapy.crawler import CrawlerRunner
+
+
+def run_crawl():
+    runner = CrawlerRunner({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+    })
+    deferred = runner.crawl(FirebasetestSpider)
+    return deferred
+
+taskcall = task.LoopingCall(run_crawl)
+taskcall.start(1800)  # call every second
+
+# l.stop() will stop the looping calls
+reactor.run()
--- zywie_pinoy_scraper/spiders/firebasetest.py
+++ zywie_pinoy_scraper/spiders/firebasetest.py
@@ -3,18 +3,6 @@ import scrapy
 import time
 import schedule
 from pyrebase import pyrebase
-config = {
-    "apiKey": "AIzaSyC6oVdbBUuBALe1l0hwcEx_E7IOw0UUEhs",
-    "authDomain": "pythonproject-e0183.firebaseapp.com",
-    "databaseURL": "https://pythonproject-e0183.firebaseio.com",
-    "projectId": "pythonproject-e0183",
-    "storageBucket": "pythonproject-e0183.appspot.com",
-    "messagingSenderId": "878861089919"
-}
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-user = auth.sign_in_with_email_and_password('admin@zywie.com', 'admin123')
-db = firebase.database()
 
 
 class FirebasetestSpider(scrapy.Spider):
@@ -23,15 +11,23 @@ class FirebasetestSpider(scrapy.Spider):
     allowed_domains = ['panlasangpinoy.com']
     start_urls = ['http://panlasangpinoy.com/indexes/recipe-index/']
     ################################################################
-    def pushToFirebase():
-        data = {
-            "name": "Its morning!"
-        }
-        results = db.child("users").push(data, user['idToken'])
+    config = {
+        "apiKey": "AIzaSyC6oVdbBUuBALe1l0hwcEx_E7IOw0UUEhs",
+        "authDomain": "pythonproject-e0183.firebaseapp.com",
+        "databaseURL": "https://pythonproject-e0183.firebaseio.com",
+        "projectId": "pythonproject-e0183",
+        "storageBucket": "pythonproject-e0183.appspot.com",
+        "messagingSenderId": "878861089919"
+    }
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+    user = auth.sign_in_with_email_and_password('admin@zywie.com', 'admin123')
+    db = firebase.database()
 
     def parse(self, response):
-        pass
-    schedule.every(5).seconds.do(pushToFirebase)
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
+        links = response.css('li.ei-item > h3 > a::attr(href)').extract_first()
+        data = {
+            "name": "Its morning!",
+            "link": links
+        }
+        results = self.db.child("users").push(data, self.user['idToken'])
