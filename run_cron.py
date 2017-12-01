d985d3d58f6bd554c5685afc3127cb2b1ad57cef
Kyle Yap <famasboy88@gmail.com>
2017-12-02 01:19:05 +0800

    Pushing to heroku

--- run_cron.py
+++ run_cron.py
@@ -1,40 +1,10 @@
-# from twisted.internet import reactor
-# import scrapy
-# from scrapy.crawler import CrawlerRunner
-# from scrapy.utils.log import configure_logging
+# from scrapy.crawler import CrawlerProcess
 # from zywie_pinoy_scraper.spiders.firebasetest import FirebasetestSpider
-# from apscheduler.schedulers.blocking import BlockingScheduler
+# from scrapy.utils.project import get_project_settings
+# from apscheduler.schedulers.twisted import TwistedScheduler
 #
-#
-#
-# sched = BlockingScheduler()
-#
-#
-# @sched.scheduled_job('cron', year='*', month='*', day="*", week='*', day_of_week='*', hour='*', minute="*", second="*/20")
-# def scheduled_job():
-#     configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-#     runner = CrawlerRunner()
-#     d = runner.crawl(FirebasetestSpider)
-#     d.addBoth(lambda _: reactor.stop())
-#
-#
-# reactor.run()  # the script will block here until the crawling is finished
-#
-#
+# process = CrawlerProcess(get_project_settings())
+# sched = TwistedScheduler()
+# sched.add_job(process.crawl, 'cron', args=[FirebasetestSpider], year='*', month='*', day="*", week='*', day_of_week='*', hour='*', minute="*/5")
 # sched.start()
-#
-# # in the main thread:
-# reactor.stop()
-#
-# # in a non-main thread:
-# reactor.callFromThread(reactor.stop)
-from scrapy.crawler import CrawlerProcess
-from zywie_pinoy_scraper.spiders.firebasetest import FirebasetestSpider
-from scrapy.utils.project import get_project_settings
-from apscheduler.schedulers.twisted import TwistedScheduler
-
-process = CrawlerProcess(get_project_settings())
-sched = TwistedScheduler()
-sched.add_job(process.crawl, 'cron', args=[FirebasetestSpider], year='*', month='*', day="*", week='*', day_of_week='*', hour='*', minute="*", second="*/30")
-sched.start()
-process.start(False)    # Do not stop reactor after spider closes
+# process.start(False)
