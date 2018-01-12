5864f4dda991a0cc016f05a419675741d61d05fe
Kyle Yap <famasboy88@gmail.com>
2018-01-12 23:12:40 +0800

    Insert to 2 nodes

--- zywie_pinoy_scraper/spiders/food_with_usda.py
+++ zywie_pinoy_scraper/spiders/food_with_usda.py
@@ -17,15 +17,24 @@ for key, category in food_exchange_tag.items():
     for category_key, category_item in category.items():
         usda_kcal = category_item.get("kcal")
         usda_value_grams = category_item.get("value_grams")
+        cnt = 0
         for food_key, food_details in food_list.items():
             list_recipe = food_details.get("list_recipe")
+            photo_url = food_details.get("photo_url")
+            if(photo_url is None):
+                photo_url = "http://images.media-allrecipes.com/global/recipes/nophoto/nopicture-910x511.png"
             if(list_recipe is not None):
                 r = re.compile("\\b" + category_key + "\\b")
                 result = filter(r.search, list_recipe)
                 if (list(result)):
                     # print("FoodEx: "+key, "cat: "+category_key, "id: "+food_key,list_recipe, "KCal: "+str(usda_kcal), "perG: "+ str(usda_value_grams))
-                    db.reference().child("foodex_usda").child(key).child(category_key).child(food_key).update({
-                        "list_recipe": list_recipe,
+                    cnt+=1
+                    db.reference().child("foodex_usda").child(key).child(category_key).update({
                         "kcal": usda_kcal,
-                        "value_grams": usda_value_grams
+                        "value_grams": usda_value_grams,
+                        "no_food_items": cnt
+                    })
+                    db.reference().child("food_each_category").child(key).child(category_key).child(food_key).update({
+                        "list_recipe": list_recipe,
+                        "photo_url": photo_url
                     })
